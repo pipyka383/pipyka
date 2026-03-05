0dd80bd543455b5814befb6394e365026c02fb3d
pipyka383 <tvetkov2007@icloud.com>
2026-03-05 17:08:10 +0300

    Add files via upload
    хелой

--- src/components/TaskMeneger.jsx
+++ src/components/TaskMeneger.jsx
@@ -1,29 +1,44 @@
 import { useState } from "react";
+
 export default function TaskManager() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks") || "[]"));
   const [input, setInput] = useState("");
 
+// сохраняет
+  const saveTasks = (newTasks) => {
+    setTasks(newTasks);
+    localStorage.setItem('tasks', JSON.stringify(newTasks));
+  };
+// добвляет 
   const addTask = (e) => {
     e.preventDefault();
     if (!input) return;
-    setTasks([...tasks, { text: input, done: false }]);
+    saveTasks([...tasks, { text: input, done: false }]);
     setInput("");
   };
-
+// галочки
   const toggle = (i) => {
     const newTasks = [...tasks];
     newTasks[i].done = !newTasks[i].done;
-    setTasks(newTasks);
+    saveTasks(newTasks);
+  };
+// удаляет 
+  const deleteTask = (i) => {
+    saveTasks(tasks.filter((_, index) => index !== i));
+  };
+// удаляет все
+  const clearAll = () => {
+    saveTasks([]);
   };
 
   return (
     <>
       <form onSubmit={addTask}>
         <input value={input} onChange={(e) => setInput(e.target.value)} />
-        <button type="submit">add</button>
+        <button type="submit">добавить</button>
       </form>
       
-      <button onClick={() => setTasks([])}>Очистить все</button>
+      <button onClick={clearAll}>Очистить все</button>
       
       <ul>
         {tasks.map((t, i) => (
@@ -32,6 +47,7 @@ export default function TaskManager() {
               {t.text}
             </span>
             <input type="checkbox" checked={t.done} onChange={() => toggle(i)} />
+            <button onClick={() => deleteTask(i)}>удалить</button>
           </li>
         ))}
       </ul>
